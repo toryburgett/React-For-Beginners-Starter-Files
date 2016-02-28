08a0ef5f1f5f69d16ce999363e0064d75d9a721b
Tory Burgett <vdburgett@gmail.com>
2016-02-28 14:52:09 -0500

    lesson 5, components for header order and inventory

--- 01 - Introduction - Start Here/scripts/main.js
+++ 01 - Introduction - Start Here/scripts/main.js
@@ -2,6 +2,71 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 /*
+  App
+*/
+
+var App = React.createClass({
+  render : function(){
+    return(
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+          {/* <Fish />
+          <Fish />
+          <Fish />
+          <Fish /> */} 
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+});
+
+/*
+  Header
+  <Header />
+*/
+var Header = React.createClass({
+  render : function(){
+    return(
+      <div>
+        <p>Header</p>
+      </div>
+    )
+  }
+});
+
+/*
+  Order
+  <Order />
+*/
+var Order = React.createClass({
+  render : function(){
+    return(
+      <div>
+        <p>Order</p>
+      </div>
+    )
+  }
+});
+
+/*
+  Inventory
+  <Inventory />
+*/
+var Inventory = React.createClass({
+  render : function(){
+    return(
+      <div>
+        <p>Inventory</p>
+      </div>
+    )
+  }
+});
+
+
+/*
   StorePicker
   This will let us make <StorePicker/>
 */
@@ -20,4 +85,4 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
